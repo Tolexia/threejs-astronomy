a8aed2d76a0379ce4c182ea1d52b1abe2a683ec9
Axel C <axel.croize@hotmail.fr>
2024-09-29 19:43:34 +0200

    Fix moon rotation

--- main.js
+++ main.js
@@ -342,7 +342,7 @@ function setPositionInOrbit(object3D, OrbitalPeriod)
   z = Number.parseFloat(z.toFixed(COORDINATE_PRECISION));
 
   object3D.position.set(x, 0, z);
-  moon.rotation.y += _degreesToRotate;
+  moon.rotation.y -= _degreesToRotate;
 }
 const tick = () => {
 
